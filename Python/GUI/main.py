6106f73684838876ae95b389ca64a3afffd6476f
jan2nov <jan.novotny@physics.slu.cz>
2022-06-12 10:05:26 +0000

    start of create input file; later different way?

--- Python/GUI/main.py
+++ Python/GUI/main.py
@@ -8,26 +8,52 @@ import os
 # Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
 sg.theme('DarkAmber')
 
-layout = [  [sg.Text('Some text on Row 1')],
-            [sg.Text('Enter something on Row 2'), sg.InputText()],
+layout = [  [sg.Text('Select filterbank: '), sg.Input(size=(30,1)), sg.FileBrowse(key='filterbank')],
+            [sg.Text('DM trials range')],
+            [sg.Text('from: '), sg.InputText(size=(6,1),key="from0"), 
+             sg.Text('to: '),   sg.InputText(size=(6,1),key="to0"), 
+             sg.Text('with DM step'), sg.InputText(size=(6,1),key="dmstep0")
+            ],
+            [sg.Text(size=(40,1), key='-OUTPUT-')],
             [sg.Button('Ok'), sg.Button('Cancel')],
             [sg.Button('Launch')]
         ]
 
+def create_input_files():
+    f = open("astroaccelerate_input_file.txt","w")
+    f.write("range\t" + 
+            values['from0'] + "\t" + 
+            values['to0'] + "\t" +
+            values['dmstep0'] + "\n"
+            )
+    f.write("file" + "\t" + values['filterbank'])
+    f.close()
+    #print('You entered ', values['from0'], values['to0'], values['dmstep0'])
+
+
+def AstroAccelerate_launch():
+    status = "Create AA input file"
+    print("Starting AstroAccelerate")
+    create_input_files()
+    status = "Starting AA" 
+    cmd = 'cat astroaccelerate_input_file.txt' #'./astro-accelerate ska_test_file-small.txt'
+    AA_return_value = os.system(cmd)
+    print("\nAstroAccelerate finished with exit code: ", AA_return_value)
+
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
     # Create the Window
-    window = sg.Window('AstroAccelerate -- Simple GUI -- testing ', layout)
+    window = sg.Window('AstroAccelerate -- Simple GUI -- testing ', layout) #.Finalize()
+#   window.Maximize()
 
     while True:
         event, values = window.read()
         if event == sg.WIN_CLOSED or event == 'Cancel':  # if user closes window or clicks cancel
             break
         if event == 'Launch':
-            cmd = 'ls -l'
-            os.system(cmd)
-            print("Starting AstroAccelerate")
-        print('You choose event', event)
-        print('You entered ', values[0])
+            AstroAccelerate_launch()
+        window['-OUTPUT-'].update("Range 0: " + values['from0'] + 
+                " to " + values['to0'] + 
+                " with DM step " + values['dmstep0'], text_color='red')
 
     window.close()
